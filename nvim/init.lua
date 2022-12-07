2763ae3fbe70995c3622af189e6582ead7b1b86b
Artyom Kropp <artyomkropp@gmail.com>
2022-12-07 22:58:08 +0900

    add tmux, zsh, nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,19 @@
+require "empty.plugins"
+require "empty.core.options"
+require "empty.core.keys"
+require "empty.core.colorscheme"
+
+require "empty.plugins.comment"
+require "empty.plugins.lualine"
+require "empty.plugins.nvim-tree"
+require "empty.plugins.telescope"
+
+require "empty.plugins.nvim-cmp"
+require "empty.plugins.lsp.mason"
+require "empty.plugins.lsp.lspsaga"
+require "empty.plugins.lsp.lspconfig"
+require "empty.plugins.lsp.null-ls"
+
+require "empty.plugins.autopairs"
+require "empty.plugins.treesitter"
+require "empty.plugins.gitsigns"
